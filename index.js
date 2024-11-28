d940021d712650ac2fdf48a499743798987e7fff
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-28 15:33:29 +0100

    function print indefinite number of arguments (with spread and rest operators)

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello " + (process.argv[2] ?? "user undefined"));
+console.log("Hello " + (process.argv[2] ?? "user undefined") + "\n");
+
+printArgs(...process.argv);
+
+function printArgs(arg0, arg1, ...rest) {
+    try {
+        if (!rest.length) throw new Error("No data");
+        rest.forEach((arg) => console.log(arg));
+    } catch (e) {
+        console.error(e);
+    }
+}
