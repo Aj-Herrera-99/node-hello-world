162f1a75ba29845ed14e2f2245da755c80e54401
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-28 16:56:02 +0100

    script "test" remove, testing ajax requests with axios

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 console.log("Hello " + (process.argv[2] ?? "user undefined") + "\n");
 
 printArgs(...process.argv);
@@ -9,4 +11,28 @@ function printArgs(arg0, arg1, ...rest) {
     } catch (e) {
         console.error(e);
     }
+}
+
+console.log();
+const myData = getData("https://jsonplaceholder.typicode.com/poss?", {
+    params: {
+        _limit: 5,
+    },
+});
+
+myData.then((data) => printTitles(data)).catch((e) => console.log(e));
+
+async function getData(path, params) {
+    try {
+        const res = await axios.get(path, params);
+        const myData = res.data;
+        if (!myData.length) throw new Error("No data found");
+        return myData;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+function printTitles(arrObjs) {
+    arrObjs?.forEach((obj) => console.log(obj?.title));
 }
